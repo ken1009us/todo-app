ffd5cd23413c4a946fdff0e51c9da291ae2df31c
Ken <ken.wu@dimaresaas.com>
2025-05-29 22:36:46 +0900

    fix

--- src/index.ts
+++ src/index.ts
@@ -63,8 +63,7 @@ app.delete('/api/todos/:id', authenticateToken, async (req: AuthRequest, res) =>
 const frontendPath = path.join(process.cwd(), 'public');
 
 app.use(express.static(frontendPath));
-
-app.get('*', (_, res) => {
+app.get('/{*any}', (_, res) => {
   res.sendFile(path.join(frontendPath, 'index.html'));
 });
 
